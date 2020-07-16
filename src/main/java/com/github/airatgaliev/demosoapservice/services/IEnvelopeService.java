97729a993d1231e8ac9739cd62726f2bc3dda3a7
Airat Galiev <airat.galiev@gmail.com>
2020-07-16 12:49:16 +0300

    edited IEnvelopeService service

--- src/main/java/com/github/airatgaliev/demosoapservice/services/IEnvelopeService.java
+++ src/main/java/com/github/airatgaliev/demosoapservice/services/IEnvelopeService.java
@@ -1,5 +1,15 @@
 package com.github.airatgaliev.demosoapservice.services;
 
+import com.github.airatgaliev.demosoapservice.payload.EnvelopeRequest;
+import com.github.airatgaliev.demosoapservice.payload.EnvelopeResponse;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElement;
+
+@WebService(name = "IPersonService")
 public interface IEnvelopeService {
 
+  @WebMethod
+  EnvelopeResponse getEarnings(@XmlElement(required = true) EnvelopeRequest envelopRequest);
 }
+
