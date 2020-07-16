73ada70776f030a14024e85fed9a546839faae82
Airat Galiev <airat.galiev@gmail.com>
2020-07-16 14:23:36 +0300

    implemented EndpointsConfig configuration

--- src/main/java/com/github/airatgaliev/demosoapservice/config/EndpointsConfig.java
+++ src/main/java/com/github/airatgaliev/demosoapservice/config/EndpointsConfig.java
@@ -1,5 +1,27 @@
 package com.github.airatgaliev.demosoapservice.config;
 
+import com.github.airatgaliev.demosoapservice.services.EnvelopeServiceImpl;
+import javax.xml.ws.Endpoint;
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class EndpointsConfig {
 
+  private final Bus bus;
+
+  @Autowired
+  public EndpointsConfig(Bus bus) {
+    this.bus = bus;
+  }
+
+  @Bean
+  public Endpoint getEnvelopEndpoint() {
+    Endpoint endpoint = new EndpointImpl(bus, new EnvelopeServiceImpl());
+    endpoint.publish("/Envelope");
+    return endpoint;
+  }
 }
