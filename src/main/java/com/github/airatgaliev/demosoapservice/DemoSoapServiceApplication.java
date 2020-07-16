41735861677528efb0ed604fd3224acb5fab4113
Airat Galiev <airat.galiev@gmail.com>
2020-07-16 12:31:18 +0300

    start project

---
+++ src/main/java/com/github/airatgaliev/demosoapservice/DemoSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.airatgaliev.demosoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSoapServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoSoapServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/github/airatgaliev/demosoapservice/DemoSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.airatgaliev.demosoapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSoapServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
