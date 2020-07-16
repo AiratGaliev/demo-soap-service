eae1603e32e774b9f8f15e9cd778a018e07be9a9
Airat Galiev <airat.galiev@gmail.com>
2020-07-16 14:12:25 +0300

    implemented EnvelopeServiceImpl service

--- src/main/java/com/github/airatgaliev/demosoapservice/services/EnvelopeServiceImpl.java
+++ src/main/java/com/github/airatgaliev/demosoapservice/services/EnvelopeServiceImpl.java
@@ -1,5 +1,30 @@
 package com.github.airatgaliev.demosoapservice.services;
 
+import com.github.airatgaliev.demosoapservice.payload.EnvelopeRequest;
+import com.github.airatgaliev.demosoapservice.payload.EnvelopeResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EnvelopeServiceImpl implements IEnvelopeService {
 
+  public static final String RESPONSE_STRING = "Чистый доход по клиенту %s %s %s составляет %s рублей!";
+
+  @Override
+  public EnvelopeResponse getEarnings(EnvelopeRequest envelopeRequest) {
+    String firstName = envelopeRequest.getFirstName().toUpperCase();
+    String secondName = envelopeRequest.getSecondName().toUpperCase();
+    String lastName = envelopeRequest.getLastName().toUpperCase();
+    Double income = envelopeRequest.getIncome();
+    Double expenses = envelopeRequest.getExpenses();
+    EnvelopeResponse envelopeResponse = new EnvelopeResponse();
+    String response = String.format(RESPONSE_STRING, lastName, firstName, secondName,
+        earningsCalculate(income, expenses).toString());
+    envelopeResponse.setResponse(response);
+    envelopeResponse.setResponseStatus("OK");
+    return envelopeResponse;
+  }
+
+  private Double earningsCalculate(Double income, Double expenses) {
+    return income - expenses;
+  }
 }
